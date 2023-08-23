553d54d202be09aabd3268957c66e6462aede6f8
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2023-08-23 01:05:13 -0400

    Successfully mounted pics using API

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemonData, setPokemonData] = useState([]);
+  
 
+  useEffect(() => {
+
+    const fetchData = async () => {
+      try{
+        let response = await fetch('https://pokeapi.co/api/v2/pokemon?offset=5&limit=5');
+
+        if(!response.ok){
+          throw new Error("Network response not ok");
+        }
+
+        let data = await response.json();
+        const urls = data.results.map(item => item.url);
+      
+        const allResponse = await Promise.all(urls.map(url => fetch(url)));
+        const allData = await Promise.all(allResponse.map(res => res.json()))
+        console.log(allData);
+
+        //update state
+        setPokemonData(allData);
+      }
+      catch(error){
+        console.error(error.message);
+      }
+    }
+
+    //Call the asyn function
+    fetchData();
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {pokemonData.map((pokemon, index) => (
+        <img key={index} src={pokemon.sprites.front_default} alt="pokemon" />
+      ))}
+    </div>
   )
 }
 
